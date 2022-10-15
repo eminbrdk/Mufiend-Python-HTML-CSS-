c59451a33cfc9c126644b4cf6f9e744c54559ca0
emininmac <mamihaha4@gmail.com>
2022-10-15 19:20:30 +0300

    database

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///mufiend.db")
 Bootstrap(app)
 
 login_manager = LoginManager()
