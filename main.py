bf41f9d3b0975a1ea032ecf9b0a3938ae29fb355
emininmac <mamihaha4@gmail.com>
2022-10-15 18:29:55 +0300

    database_url

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def admin_only(f):
 
 
 # Create DataBase
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
